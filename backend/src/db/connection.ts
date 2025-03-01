3dedefe985b37a8d9cebd01bb8f1f80e857aa995
Tatsuro Osuka <oscarwild13@gmail.com>
2025-03-01 10:26:34 +0900

    MySQL 接続、CRUD の "Create" と "Read" が正常に

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -5,7 +5,7 @@ import * as schema from './schema';
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3307, // Docker コンテナ上のポートに合わせる (例: 3307)
+    port: Number(process.env.DB_PORT) || 3306, // Docker コンテナ上のポートに合わせる (例: 3306)
     user: process.env.DB_USER || 'user',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'task_db',
