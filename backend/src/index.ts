df66a2f7ae00aa752ed3d599f62519dd92c9e8e0
Tatsuro Osuka <oscarwild13@gmail.com>
2025-03-02 10:16:25 +0900

    CORS ミドルウェアを追加し、HTTP メソッドとヘッダーを許可する設定を更新

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import tasks from "./routes/tasks";
+import { cors } from 'hono/cors'
 
 const app = new Hono();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        allowMethods: ["GET", "POST", "PUT", "DELETE"],
+        allowHeaders: ["Content-Type"],
+    })
+);
 
 app.get("/", (c) => c.text("Hello Hono!") );
 
